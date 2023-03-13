8183763feec05b52890c6c0a7062364ff004d2aa
Yarl <rich39739@gmail.com>
2023-03-14 02:05:51 +0400

    zad5

--- sem/sem2_zad5/Program.cs
+++ sem/sem2_zad5/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+Console.WriteLine("Введите первое число: ");
+
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x == y*y || y == x*x)
+{
+Console.WriteLine($" Число является квадратом ");
+
+}
+else
+{
+Console.WriteLine($" Квадрат отсутсвует ");
+}
