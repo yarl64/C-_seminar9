ed74a05a80f986fc04ea7e5bd8d7d4836b309118
Yarl <rich39739@gmail.com>
2023-03-14 01:57:30 +0400

    zad4

---
+++ sem/sem2_zad4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число ");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x % 7 == 0 && x % 23 == 0)
+{
+Console.WriteLine($" Число {x} кратно 7 и кратно 23 ");
+
+}
+else
+{
+Console.WriteLine($" Число {x} не кратно 7 и не кратно 23 ");
+}
