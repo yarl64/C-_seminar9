5695ae60278487e9380475c9b0016c05d71d54ed
Yarl <rich39739@gmail.com>
2023-03-14 01:18:16 +0400

    zad3

---
+++ sem/sem2_zad3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и
+// выводить, является ли второе число кратным первому.
+// Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+Console.WriteLine("Введите целое число");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое число");
+int y = Convert.ToInt32(Console.ReadLine());
+int remains = y % x;
+
+if (remains == 0)
+{
+    Console.WriteLine("Кратно");
+}
+
+else
+{
+    Console.WriteLine("Не кратно, остаток " + remains);
+}
